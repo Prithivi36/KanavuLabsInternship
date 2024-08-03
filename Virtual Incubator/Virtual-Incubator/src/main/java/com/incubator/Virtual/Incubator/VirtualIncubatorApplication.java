d6d906848784cd660605804d0e1ecb95844d68e7
Prithivi <prithivipappian@gmail.com>
2024-08-03 10:39:08 +0530

    Virtual incubator - Initialization

---
+++ Virtual Incubator/Virtual-Incubator/src/main/java/com/incubator/Virtual/Incubator/VirtualIncubatorApplication.java
@@ -0,0 +1,13 @@
+package com.incubator.Virtual.Incubator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualIncubatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualIncubatorApplication.class, args);
+	}
+
+}
---
+++ Virtual Incubator/Virtual-Incubator/src/test/java/com/incubator/Virtual/Incubator/VirtualIncubatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubator.Virtual.Incubator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualIncubatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
